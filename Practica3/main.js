540231bf28afa29810490d0adfe0fe746452001b
kevin solis salazar <solisske@icloud.com>
2025-05-21 20:14:06 -0600

    Aqui se hizo una funcion que resta dos numeros en la cual se importa una funcion hacia el archivo main.js y se hace una prueba de la funcion resta

---
+++ Practica3/main.js
@@ -0,0 +1,7 @@
+
+// Importar la función restar desde utils.js
+const restar = require('./utils.js');
+// Ejecutar varias pruebas
+console.log(restar(10, 5)); 
+console.log(restar(200, 15)); 
+console.log(restar(1000, 500));
---
+++ Practica3/utils.js
@@ -0,0 +1,10 @@
+/**
+ * 1. Crea un archivo llamado utils.js con una función llamada restar(a, b) que retorne la resta
+ * de dos números.
+ * 2. Crea un archivo principal (main.js), importa esa función y úsala ejecutar varias pruebas.
+ */
+function restar(a, b) {
+    return a - b;
+}
+// Exportar la función restar para que pueda ser utilizada en otros archivos    
+module.exports = restar;
